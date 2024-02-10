2415e670fec6105209bd597a14464ffb07e88ced
Matthew Berens <matthewjberens@gmail.com>
2024-02-10 15:11:35 -0500

    file upload

--- R/STIC_imports.R
+++ R/STIC_imports.R
@@ -1,8 +1,29 @@
-
-
+#' Import raw STIC sensor data
+#'
+#' @description
+#' loadSTIC() is a tool to import and tidy raw STIC sensor data.
+#' `loadSTICdir()`imports and tidies a directory containing multiple raw STIC sensor files.
+#'
+#'
+#' @import fs
+#' @import tidyr
+#' @import purrr
+#' @import lubridate
+#' @import stringr
+#' @importFrom utils read.csv
+#'
+#' @param rawSTIC A dataframe
+#' @param dirSTIC A directory with raw STIC files
+#'
+#'
+#'
+#' @return the formatted data
+#'
+#' @rdname loadSTIC
+#' @export
+#'
 
 #Update files
-
 loadSTIC <- function(rawSTIC){
   read.csv(rawSTIC) %>%
     dplyr::select(-c(1)) %>%
@@ -17,26 +38,27 @@ loadSTIC <- function(rawSTIC){
                   DateTime = mdy_hm(DateTime))
 }
 
+#' @rdname loadSTIC
+#' @export
 
-
-loadSTICdir <- function(Path){
-  Path %>%
+loadSTICdir <- function(dirSTIC){
+  dirSTIC %>%
     dir_ls(regexp = "\\.csv$") %>%
     map_dfr(read_csv, .id = "FileID", show_col_types = FALSE) %>%
     dplyr::select(-"#") %>%
-    dplyr::rename_with(.cols = contains("Date"),
+    dplyr::rename_with_(.cols = contains("Date"),
                        .fn = function(x){"DateTime"}) %>%
-    dplyr::rename_with(.cols = contains("Temp"),
+    dplyr::rename_with_(.cols = contains("Temp"),
                        .fn = function(x){"TEMP_C"}) %>%
-    dplyr::rename_with(.cols = contains("Intensity"),
+    dplyr::rename_with_(.cols = contains("Intensity"),
                        .fn = function(x){"INTENSITY_LUX"}) %>%
-    dplyr::mutate(TEMP_C = as.numeric(TEMP_C),
+    dplyr::mutate_(TEMP_C = as.numeric(TEMP_C),
                   INTENSITY_LUX = as.numeric(INTENSITY_LUX),
                   DateTime = mdy_hm(DateTime)) %>%
-    dplyr::mutate(FileID = gsub(paste(P, "/", sep = ""), '', FileID),
+    dplyr::mutate_(FileID = gsub(paste(P, "/", sep = ""), '', FileID),
                   Site = str_extract(FileID, "[^_]+"),
                   STIC_ID = str_match(FileID, "_\\s*(.*?)\\s*_")[,2],
                   Group = str_extract(Site, "[^.]+")) %>%
-    dplyr::select(-"FileID") %>%
+    dplyr::select_(-"FileID") %>%
     dplyr::select(Group, Site, DateTime, INTENSITY_LUX, TEMP_C)
 }
